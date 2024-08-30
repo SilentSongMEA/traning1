8b67bc74c347c2b6c72866961aad331d1574c6e4
MohadesehAsri <58567963+SilentSongMEA@users.noreply.github.com>
2024-08-30 12:22:26 +0330

    Create emp.py

---
+++ emp.py
@@ -0,0 +1,9 @@
+class emploee:
+  def __init__(self,fn,ln,job,salary):
+    self.firstname=fn
+    self.lastname=ln
+    self.job=job
+    self.salary=salary
+  def a(self,percent):
+    return self.salary  *percent
+
